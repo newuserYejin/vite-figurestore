5efe0b3db3e9ed49023720f569996ea4b64b1290
jin <jin200126@gmail.com>
2025-04-09 02:18:50 +0900

    로그인 페이지 연결

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,8 +3,10 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser } from "@fortawesome/free-regular-svg-icons";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import logoImg from "../assets/logo.png";
+import { useNavigate } from "react-router";
 
 const Navbar = () => {
+  const navigate = useNavigate();
   const menuList = ["짱구", "커비", "스누피", "산리오"];
 
   return (
@@ -12,11 +14,11 @@ const Navbar = () => {
       <div>
         <div className="loginButton">
           <FontAwesomeIcon icon={faUser} />
-          <div>로그인</div>
+          <div onClick={() => navigate("/login")}>로그인</div>
         </div>
       </div>
       <div className="navLogo">
-        <img src={logoImg} alt="로고" />
+        <img src={logoImg} alt="로고" onClick={() => navigate("/")} />
       </div>
       <div className="menuArea">
         <ul className="menuList">
