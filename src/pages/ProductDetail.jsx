698793ad8cdbac6c7072530b35c20e3c87ddae8e
jin <jin200126@gmail.com>
2025-04-12 00:45:58 +0900

    css update

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Col, Container, Row } from "react-bootstrap";
+import { Col, Container, Placeholder, Row } from "react-bootstrap";
 import { data, useParams, useSearchParams } from "react-router";
 
 const ProductDetail = () => {
@@ -24,49 +24,66 @@ const ProductDetail = () => {
   return (
     <Container>
       <Row className="productDetailBox">
-        <Col md={6} xs={12} className="detailImg">
-          <img src={detailData?.img} alt="상품 이미지" />
-        </Col>
-        <Col md={5} xs={12} className="productDetailInfoBox">
-          <Row className="productStatus">
-            <Col
-              xs={6}
-              className="productNew"
-              style={{ display: detailData?.new ? "block" : "none" }}
-            >
-              {detailData?.new ? "신제품" : null}
+        {loading ? (
+          <>
+            <Col l md={6} xs={12}>
+              <Placeholder animation="glow">
+                <Placeholder className="placeHolderBox" xs={12} />
+              </Placeholder>
             </Col>
-            <Col
-              xs={6}
-              className="productMD"
-              style={{ display: detailData?.choice ? "block" : "none" }}
-            >
-              {detailData?.choice ? "Md choice" : null}
+            <Col l md={5} xs={12} className="loadingInfo">
+              <Placeholder animation="glow">
+                <Placeholder className="placeHolderBox" xs={12} />
+              </Placeholder>
             </Col>
-          </Row>
-          <Row>
-            <Col className="productTitle">{detailData?.title}</Col>
-          </Row>
-          <Row>
-            <Col className="productPrice">&#8361; {detailData?.price}</Col>
-          </Row>
-          <Row>
-            <Col>
-              <label htmlFor="option">선택 : </label>
-              <select name="option" id="option">
-                {detailData?.option.map((item) => (
-                  <option value={item}>{item}</option>
-                ))}
-              </select>
+          </>
+        ) : (
+          <>
+            <Col md={6} xs={12} className="detailImg">
+              <img src={detailData?.img} alt="상품 이미지" />
             </Col>
-          </Row>
+            <Col md={5} xs={12} className="productDetailInfoBox">
+              <Row className="productStatus">
+                <Col
+                  xs={6}
+                  className="productNew"
+                  style={{ display: detailData?.new ? "block" : "none" }}
+                >
+                  {detailData?.new ? "신제품" : null}
+                </Col>
+                <Col
+                  xs={6}
+                  className="productMD"
+                  style={{ display: detailData?.choice ? "block" : "none" }}
+                >
+                  {detailData?.choice ? "Md choice" : null}
+                </Col>
+              </Row>
+              <Row>
+                <Col className="productTitle">{detailData?.title}</Col>
+              </Row>
+              <Row>
+                <Col className="productPrice">&#8361; {detailData?.price}</Col>
+              </Row>
+              <Row>
+                <Col>
+                  <label htmlFor="option">선택 : </label>
+                  <select name="option" id="option">
+                    {detailData?.option.map((item) => (
+                      <option value={item}>{item}</option>
+                    ))}
+                  </select>
+                </Col>
+              </Row>
 
-          <Row>
-            <Col xs={6} className="productAddBtn">
-              <button>추가하기</button>
+              <Row>
+                <Col xs={6} className="productAddBtn">
+                  <button>추가하기</button>
+                </Col>
+              </Row>
             </Col>
-          </Row>
-        </Col>
+          </>
+        )}
       </Row>
     </Container>
   );
